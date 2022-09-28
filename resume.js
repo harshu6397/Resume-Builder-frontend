c692cb10495de6f5d3dd604757ff7ab014a2ae40
Ashu_kumar <ashukumar8472@gmail.com>
2022-09-28 18:04:55 +0530

    form updated with javascript file

--- resume.js
+++ resume.js
@@ -1,39 +1,127 @@
+// adding acadmics details
+const AddAcadmicsBTN = document.querySelector(".add-coll")
+AddAcadmicsBTN.addEventListener("click", ()=>{
+    // label and input
+    let NewExpNameLabel1 = document.createElement("label")
+    NewExpNameLabel1.setAttribute("for","course");
+    NewExpNameLabel1.innerHTML = "Course"
+    let NewExpNameInput1 = document.createElement("input")
+    NewExpNameInput1.setAttribute("type","text");
+    NewExpNameInput1.classList.add("course")
+
+    // 2
+    let NewExpNameLabel2 = document.createElement("label")
+    NewExpNameLabel2.setAttribute("for","coll-branch");
+    NewExpNameLabel2.innerHTML = "Branch"
+    let NewExpNameInput2 = document.createElement("input")
+    NewExpNameInput2.setAttribute("type","text");
+    NewExpNameInput2.classList.add("coll-branch")
+
+    // 3
+    let NewExpNameLabel3 = document.createElement("label")
+    NewExpNameLabel3.setAttribute("for","coll");
+    NewExpNameLabel3.innerHTML = "School/College Name and Address"
+    let NewExpNameInput3 = document.createElement("input")
+    NewExpNameInput3.setAttribute("type","text");
+    NewExpNameInput3.classList.add("coll")
+
+    let AcadmicBox = document.querySelector(".academic-detail")
+
+    AcadmicBox.insertBefore( NewExpNameLabel1, AddAcadmicsBTN)
+    AcadmicBox.insertBefore( NewExpNameInput1, AddAcadmicsBTN)
+    
+    AcadmicBox.insertBefore( NewExpNameLabel2, AddAcadmicsBTN)
+    AcadmicBox.insertBefore( NewExpNameInput2, AddAcadmicsBTN)
+    
+    AcadmicBox.insertBefore( NewExpNameLabel3, AddAcadmicsBTN)
+    AcadmicBox.insertBefore( NewExpNameInput3, AddAcadmicsBTN)
+    
+})
+
 // adding project
 const projectBTN = document.querySelector(".add-pj")
-projectBTN.addEventListener("click", addProject);
+projectBTN.addEventListener("click", ()=>{
+    // label and input
+    let NewExpNameLabel = document.createElement("label")
+    NewExpNameLabel.setAttribute("for","project");
+    NewExpNameLabel.innerHTML = "Project title"
+    let NewExpNameInput = document.createElement("input")
+    NewExpNameInput.setAttribute("type","text");
+    NewExpNameInput.classList.add("project")
 
-function addProject(){
-    console.log("avs")
-    let newPjLabelForTitle = document.createElement("label");
-    let newPjInp = document.createElement("input");
-    let newPjLabelForDisc = document.createElement("label");
-    let newPjDisc = document.createElement("textarea");
-    newPjInp.classList.add("project");
-    newPjLabelForTitle.setAttribute("for","project");
-    newPjInp.setAttribute("type","text");
-    newPjDisc.classList.add("project-dc");
-    newPjLabelForDisc.setAttribute("for","project-dc");
-    newPjInp.setAttribute("cols","20");
-    newPjInp.setAttribute("rows","5");
-}
 
-// addingDetails();
-// function addingDetails() {
-//     let fName = document.getElementById("fname").value;
-//     let firstName = document.getElementById("firstName");
-//     firstName.innerHTML = fName;
-// }
+    //label and textarea
+    let NewExpNameLabelD = document.createElement("label")
+    NewExpNameLabelD.setAttribute("for","project-dc");
+    NewExpNameLabelD.innerHTML = "Project-discription"
+    let NewExpNameTxt = document.createElement("textarea")
+    NewExpNameTxt.setAttribute("row","5");
+    NewExpNameTxt.classList.add("project-dc") 
+
+    let expBox = document.querySelector(".projBox")
+    expBox.appendChild(NewExpNameLabel)
+    expBox.appendChild(NewExpNameInput)
+    expBox.appendChild(NewExpNameLabelD)
+    expBox.appendChild(NewExpNameTxt)
+});
+
+// adding experience
+const ExpBTN = document.querySelector(".add-ex")
+ExpBTN.addEventListener("click", ()=>{
+    // label and input
+    let NewExpNameLabel = document.createElement("label")
+    NewExpNameLabel.setAttribute("for","exp");
+    NewExpNameLabel.innerHTML = "Company Name"
+    let NewExpNameInput = document.createElement("input")
+    NewExpNameInput.setAttribute("type","text");
+    NewExpNameInput.classList.add("exp")
+
+
+    //label and textarea
+    let NewExpNameLabelD = document.createElement("label")
+    NewExpNameLabelD.setAttribute("for","exp-dc");
+    NewExpNameLabelD.innerHTML = "Describe role"
+    let NewExpNameTxt = document.createElement("textarea")
+    NewExpNameTxt.setAttribute("row","5");
+    NewExpNameTxt.classList.add("exp-dc") 
+
+    let expBox = document.querySelector(".expBox")
+    expBox.appendChild(NewExpNameLabel)
+    expBox.appendChild(NewExpNameInput)
+    expBox.appendChild(NewExpNameLabelD)
+    expBox.appendChild(NewExpNameTxt)
+})
+
+
+//adding certificate
+const CertBTN = document.querySelector(".add-cert")
+CertBTN.addEventListener("click", ()=>{
+    let certBox = document.querySelector(".certficate-detail")
+    let newCert = document.createElement("input")
+    newCert.classList.add('cert')
+    certBox.insertBefore(newCert, CertBTN);
+})
+
+
+// adding achievement input box
+let achiveBox = document.querySelector(".achive")
+const addAchivBtn = document.querySelector(".add-achiv");
+addAchivBtn.addEventListener("click", () => {
+    let newAchive = document.createElement("input")
+    newAchive.classList.add('achiv')
+    achiveBox.insertBefore(newAchive, addAchivBtn);
+})
 
 const submit = document.querySelector(".btn-submit");
-submit.addEventListener("click", ()=>{
+submit.addEventListener("click", () => {
     console.log("asdf")
 
     //email
     document.getElementById("email-d").innerHTML = document.getElementById("email").value
-    
-    //domain
-    // document.getElementById("domain-d").innerHTML = document.getElementById("domain").value
-    
+
+    domain
+    document.getElementById("domain-d").innerHTML = document.getElementById("domain").value
+
     //phone
     document.getElementById("phone-d").innerHTML = document.getElementById("phone").value
 
@@ -45,7 +133,7 @@ submit.addEventListener("click", ()=>{
 
     //language
     document.getElementById("lang").innerHTML = document.getElementById("language").value
-    
+
     //dob
     document.getElementById("dob-d").innerHTML = document.getElementById("dobIn").value
 
@@ -61,23 +149,23 @@ submit.addEventListener("click", ()=>{
     document.getElementById("profile-d").innerHTML = document.getElementById("profile").value
 
     // education
-    document.getElementById("course").innerHTML = document.querySelector(".coll").value
-    document.getElementById("coll-d").innerHTML = document.querySelector(".coll-add").value
+    document.getElementById("course").innerHTML = document.querySelector(".course").value
+    document.getElementById("coll-d").innerHTML = document.querySelector(".coll").value
 
     // company name
-    document.getElementById("org").innerHTML = document.querySelector(".exp").value
+    document.getElementById("org").innerHTML = document.getElementById("exp").value
     document.getElementById("org-d").innerHTML = document.querySelector(".exp-dc").value
-    
+
     // project name
     document.getElementById("proj").innerHTML = document.querySelector(".project").value
     document.getElementById("proj-d").innerHTML = document.querySelector(".project-dc").value
-    
+
     // certification
-    document.getElementById("certificate").innerHTML = document.getElementById("cert").value
-    
+    document.getElementById("certificate").innerHTML = document.getElementById("cert")
+
     // achivement
-    document.getElementById("achivem").innerHTML = document.getElementById("achiv").value
-    
+    document.getElementById("achivem").innerHTML = document.querySelector(".achiv").value
+
 
 })
 
